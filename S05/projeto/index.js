3baf400d75c56b6ee62306ee24a66affdd936ac4
Vinicius Figueiredo <vifigcode@gmail.com>
2022-09-21 13:38:45 -0300

    seção 5 -> iniciando form de perguntas

--- S05/projeto/index.js
+++ S05/projeto/index.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/perguntar', (req, res) => {
+    res.render('perguntar');
+});
+
 // Config server
 const porta = 8080;
 app.listen(porta, () => {
