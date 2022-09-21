04eccb9e08c5e87355a139cd25e09af37be86e11
Vinicius Figueiredo <vifigcode@gmail.com>
2022-09-20 23:23:03 -0300

    seção 3 - rotas express

--- S03/index.js
+++ S03/index.js
@@ -1,6 +1,21 @@
 const express = require('express'); // importando express
 const app = express(); // inicializando express
 
+// qual url e qual a rota
+// rota raiz
+// uma resposta (obrigatoria) por rota
+app.get('/', (req, res) => {
+    res.send('Bem-vindo a página inicial.')
+})
+
+app.get('/blog', (req, res) => {
+    res.send('Bem-vindo ao blog.')
+})
+
+app.get('/canal/youtube', (req, res) => {
+    res.send('Bem-vindo ao meu canal do youtube.')
+})
+
 const porta = 4000;
 app.listen(porta,(erro) => {
     if(erro) console.log('Ocorreu um erro.');
