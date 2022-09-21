1012cf0a65b5e1ac4ff21070efb2cc5ade95c00a
Vinicius Figueiredo <vifigcode@gmail.com>
2022-09-21 13:19:40 -0300

    seção 5 -> arquivos estáticos e inicio do projeto

--- S05/example/index.js
+++ S05/example/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs'); // informa qual o template engine a ser utilizado (pasta views por padrão)
+app.use(express.static('public')); // arquivos estáticos que não de responsabilidade do backend
 
 // Rota -> Página Inicial
 // views/index.ejs
---
+++ S05/projeto/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs'); // informa qual o template engine a ser utilizado (pasta views por padrão)
+app.use(express.static('public')); // arquivos estáticos que não de responsabilidade do backend
+
+// Rota -> Página Inicial
+// views/index.ejs
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// Config server
+const porta = 8080;
+app.listen(porta, () => {
+    console.log(`App executando na porta: http://localhost:${porta}.`);
+});
