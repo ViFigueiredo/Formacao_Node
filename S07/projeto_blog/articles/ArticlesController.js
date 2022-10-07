0c26e7bf9aa6103d2287adb520559cf67575d9f0
Vinicius Figueiredo <vifigcode@gmail.com>
2022-10-07 15:29:50 -0300

    cadastro de artigos, editor de texto e bd

--- S07/projeto_blog/articles/ArticlesController.js
+++ S07/projeto_blog/articles/ArticlesController.js
@@ -3,12 +3,36 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/articles', (req, res) => {
+const Category = require('../categories/Category');
+const Article = require('../articles/Article');
+const sluggify = require('slugify');
+
+router.get('/admin/articles', (req, res) => {
     res.send('ROTA DE ARTIGOS');
 });
 
 router.get('/admin/articles/new', (req, res) => {
-    res.send('ROTA PARA CRIAR UM NOVO ARTIGO');
+    Category.findAll().then(categories => {
+        res.render('admin/articles/new', { categories });
+    });
+});
+
+router.post('/articles/save', (req, res) => {
+
+    // capturando dados de formulário
+    let title = req.body.title;
+    let body = req.body.body;
+    let categoryId = req.body.category;
+
+    // adicionando dados ao model/db
+    Article.create({
+        title,
+        slug: sluggify(title),
+        body,
+        categoryId // chave estrangeira
+    }).then(() => {
+        res.redirect('/admin/articles')
+    })
 });
 
 module.exports = router;
--- S07/projeto_blog/database/database.js
+++ S07/projeto_blog/database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 const connection = new Sequelize('guiapress', 'root', 'dev', {
     host: '130.211.226.255',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezoe: '-03:00'
 });
 
 module.exports = connection;
