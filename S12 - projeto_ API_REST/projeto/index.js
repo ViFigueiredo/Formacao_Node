b56e33ea8d82df411f7b7861006aaf8f2986095b
Vinicius Figueiredo <vifigcode@gmail.com>
2022-10-24 16:04:08 -0300

    teste de verbos http com api rest

--- S12 - projeto_ API_REST/projeto/index.js
+++ S12 - projeto_ API_REST/projeto/index.js
@@ -8,19 +8,19 @@ app.use(bodyParser.json());
 var DB = {
     games: [
         {
-            id: 01,
+            id: 1,
             title: "Call of Duty Warzone",
             year: 2019,
             price: 60
         },
         {
-            id: 02,
+            id: 2,
             title: "ARK Survive Evolved",
             year: 2017,
             price: 80
         },
         {
-            id: 03,
+            id: 3,
             title: "Minecraft",
             year: 2012,
             price: 20
@@ -28,8 +28,76 @@ var DB = {
     ]
 }
 
-app.get('/', () => {
+app.get('/games', (req, res) => {
+    res.statusCode = 200;
+    res.json(DB.games);
+})
 
+app.get('/game/:id', (req, res) => { // listar
+
+    var id = parseInt(req.params.id);
+
+    if (isNaN(id)) {
+        res.sendStatus(400);
+    } else {
+        var game = DB.games.find(game => game.id == id);
+
+        if (game !== undefined) {
+            res.statusCode = 200;
+            res.send(game);
+        } else {
+            res.sendStatus(404);
+        }
+    }
+});
+
+app.post('/game', (req, res) => { // criar
+    var { id, title, price, year } = req.body;
+
+    DB.games.push({ id, title, price, year });
+    res.sendStatus(200);
+});
+
+app.delete('/game/:id', (req, res) => { // deletar
+
+    if (isNaN(req.params.id)) {
+        res.sendStatus(400);
+    } else {
+        var id = parseInt(req.params.id);
+        var gameIndex = DB.games.findIndex(game => game.id == id);
+
+        if (gameIndex == -1) {
+            res.sendStatus(404);
+        } else {
+            DB.games.splice(gameIndex, 1);
+            res.sendStatus(200);
+        }
+    }
+});
+
+app.put('/game/:id', (req, res) => { // editar
+
+    if (isNaN(req.params.id)) {
+        res.sendStatus(400);
+    } else {
+        var id = parseInt(req.params.id);
+        var game = DB.games.find(game => game.id == id);
+
+        if (game != undefined) {
+
+            var { id, title, price, year } = req.body;
+
+            if (id != undefined) game.id = id;
+            if (title != undefined) game.title = title;
+            if (price != undefined) game.price = price;
+            if (year != undefined) game.year = year;
+
+            res.sendStatus(200);
+
+        } else {
+            res.sendStatus(404);
+        }
+    }
 })
 
 const port = 3000;
