a1cd5e9df55ada33a4de55b832b02144461542a8
Vinicius Figueiredo <vifigcode@gmail.com>
2022-10-27 14:15:47 -0300

    init, data bind, interpolation, props

--- S20 - Vue.js/src/App.vue
+++ S20 - Vue.js/src/App.vue
@@ -1,91 +1,37 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
+<!-- Componentes
+Para renderizar uma componente, basta importá-lo em script informando no export a qual component do app ele pertence
+ -->
+
+<!-- Props
+Podemos passar props="" como valores dinâmicos de componentes
+ -->
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div id="app">
+    <h1>Guia Clientes</h1>
+    <Cliente propNome="Vinicius Figueiredo" propIdade="28" />
+    <Cliente :propNome="apelido" propIdade="24" />
+    <Cliente propNome="Rafael Prezotti" propIdade="16" />
+    <Cliente propNome="Davi Izoton" propIdade="10" />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script>
+import Cliente from "./components/Cliente.vue";
+// import Produto from "./components/Produto.vue";
+export default {
+  name: "App",
+  data() {
+    return {
+      apelido: "Boca rosa", // variáveis reativas
+    };
+  },
+  components: {
+    Cliente,
+    // Produto,
+  },
+};
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<!-- scoped - apenas para esse componente(arquivo) -->
+<style scoped></style>
