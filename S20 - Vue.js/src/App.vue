ae4e58898fec993e706e0be3480a298c749749f0
Vinicius Figueiredo <vifigcode@gmail.com>
2022-10-27 15:14:10 -0300

    condicionais

--- S20 - Vue.js/src/App.vue
+++ S20 - Vue.js/src/App.vue
@@ -9,21 +9,33 @@ Podemos passar props="" como valores dinâmicos de componentes
 <template>
   <div id="app">
     <h1>Guia Clientes</h1>
-    <Cliente propNome="Vinicius Figueiredo" propIdade="28" />
-    <Cliente :propNome="apelido" propIdade="24" />
-    <Cliente propNome="Rafael Prezotti" propIdade="16" />
-    <Cliente propNome="Davi Izoton" propIdade="10" />
+    <input type="text" v-model="clienteObj.nome" />
+    <!-- data.objeto também funciona com v-model -->
+    <!-- <Cliente propNome="Vinicius Figueiredo" propIdade="28" />
+    <Cliente :propNome="apelido" propIdade="24" /> -->
+
+    <!-- sempre que for passado uma prop como js, devemos bindar -->
+    <Cliente :cliente="clienteObj" :showIdade="true" />
+    <Cliente :cliente="clienteObj" />
+
+    <!-- <Cliente propNome="Davi Izoton" propIdade="10" /> -->
   </div>
 </template>
 
 <script>
-import Cliente from "./components/Cliente.vue";
+import Cliente from "./components/Cliente_ex2.vue";
+// import Cliente from "./components/Cliente_ex1.vue";
 // import Produto from "./components/Produto.vue";
 export default {
   name: "App",
   data() {
     return {
-      apelido: "Boca rosa", // variáveis reativas
+      // apelido: "Boca rosa", // variáveis reativas
+      clienteObj: {
+        nome: "Rafa",
+        email: "rafa@email.com",
+        idade: 16,
+      },
     };
   },
   components: {
---
+++ S20 - Vue.js/src/components/Cliente_ex2.vue
@@ -0,0 +1,41 @@
+<!-- Estrutura Básica 
+Um componente raiz só pode ter uma tag filha, de outro modo, devemos envolver tudo em uma tag <div>
+-->
+
+<!-- Reatividade
+Toda vez que que um dado muda, todas suas referências também mudam.
+ -->
+
+<template>
+  <div id="cliente">
+    <h4>Nome: {{ cliente.nome }}</h4>
+    <hr />
+    <p>E-mail: {{ cliente.email }}</p>
+
+    <!-- bloco de condicionais -->
+    <p v-if="showIdade">Idade: {{ cliente.idade }}</p>
+    <p v-else>O usuário escondeu sua idade.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    // valores dinâmicos para componentes
+    cliente: Object,
+    showIdade: Boolean,
+  },
+};
+</script>
+
+<style scoped>
+#cliente {
+  background-color: #ece5e3;
+  max-width: 600px;
+  padding: 1%;
+  margin-bottom: 2%;
+}
+</style>
