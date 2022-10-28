cbf4cbfe1fcbfe7f2802082bdb0fad49e5678f90
Vinicius Figueiredo <vifigcode@gmail.com>
2022-10-28 14:01:11 -0300

    axios

--- S21 - projeto_pokedex_vuejs/src/App.vue
+++ S21 - projeto_pokedex_vuejs/src/App.vue
@@ -3,8 +3,24 @@
 </template>
 
 <script>
-// import axios from "axios";
-export default {};
+import axios from "axios";
+export default {
+  // retorna um obj
+  data() {
+    return {
+      pokemons: [],
+    };
+  },
+  // executa em todo load de página
+  created: function () {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then((res) => {
+        console.log("Pegou a lista de pokemons.");
+        this.pokemons = res.data.results;
+      });
+  },
+};
 </script>
 
 <style></style>
