bec19b17176846049173ab58f9d0557aaa2f9edb
Vinicius Figueiredo <vifigcode@gmail.com>
2022-10-28 17:04:42 -0300

    barra de pesquisa

--- S21 - projeto_pokedex_vuejs/src/App.vue
+++ S21 - projeto_pokedex_vuejs/src/App.vue
@@ -4,7 +4,24 @@
       <img class="logo" src="./assets/img/logo.svg" alt="" />
     </div>
     <div class="column is-half is-offset-one-quarter">
-      <div v-for="(poke, index) in pokemons" :key="index">
+      <div class="search-bar">
+        <input
+          type="text"
+          class="input is-rounded"
+          placeholder="Buscar Pokémon pelo nome:"
+          v-model="busca"
+        />
+        <button class="button is-warning is-rounded" @click="buscar">
+          Buscar
+        </button>
+      </div>
+      <hr />
+
+      <!-- sem barra de busca -->
+      <!-- <div v-for="(poke, index) in pokemons" :key="index"> -->
+      <!-- com barra de busca -->
+      <!-- <div v-for="(poke, index) in resultadoBusca" :key="index"> -->
+      <div v-for="(poke, index) in filteredPokemons" :key="poke.url">
         <Pokemon :name="poke.name" :url="poke.url" :num="index + 1" />
       </div>
     </div>
@@ -20,6 +37,8 @@ export default {
   data() {
     return {
       pokemons: [],
+      busca: "",
+      filteredPokemons: [],
     };
   },
   // executa em todo load de página
@@ -29,10 +48,35 @@ export default {
       .then((res) => {
         console.log("Pegou a lista de pokemons.");
         this.pokemons = res.data.results;
+        this.filteredPokemons = res.data.results;
       });
   },
   components: {
     Pokemon,
+  },
+  methods: {
+    buscar: function () {
+      this.filteredPokemons = this.pokemons;
+      if (this.busca == "" || this.busca == " ") {
+        this.filteredPokemons = this.pokemons;
+      } else {
+        this.filteredPokemons = this.pokemons.filter(
+          (pokemon) => pokemon.name.toLowerCase() == this.busca.toLowerCase()
+        );
+        // this.filteredPokemons = this.pokemons.filter(
+        //   (pokemon) => pokemon.name == this.busca
+        // );
+      }
+    },
+  },
+  computed: {
+    // resultadoBusca: function () {
+    //   if (this.busca == "" || this.busca == " ") {
+    //     return this.pokemons;
+    //   } else {
+    //     return this.pokemons.filter((pokemon) => pokemon.name == this.busca);
+    //   }
+    // },
   },
 };
 </script>
@@ -50,5 +94,20 @@ export default {
 
 .logo {
   max-width: 300px;
+}
+
+.search-bar {
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+
+.search-bar input {
+  width: 75%;
+}
+
+.search-bar button {
+  width: 20%;
 }
 </style>
