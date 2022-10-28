dd57277d9fbcd09d2adf1dd066527d84cef9565d
Vinicius Figueiredo <vifigcode@gmail.com>
2022-10-28 09:55:14 -0300

    eventos

---
+++ S20 - Vue.js/08_eventos/src/components/Cliente.vue
@@ -0,0 +1,59 @@
+<template>
+  <!-- classes com condicionais + if/else -->
+  <div :class="{ cliente: !isPremium, clientePremium: isPremium }">
+    <h4>Nome: {{ cliente.nome }}</h4>
+    <hr />
+    <p>E-mail: {{ cliente.email }}</p>
+    <p v-if="showIdade">Idade: {{ cliente.idade }}</p>
+
+    <!-- v-else entende o último v-if utilizado / não renderiza no DOM -->
+    <p v-else>O usuário escondeu sua idade.</p>
+
+    <!-- v-show entende o último v-if utilizado / renderiza no DOM -->
+    <!-- <p v-show>O usuário escondeu sua idade.</p> -->
+
+    <!-- v-else-if entende o último v-if utilizado / não renderiza no DOM -->
+    <!-- <p v-else-if="showIdade === false">Um bloco de else-if.</p> -->
+
+    <button @click="mudarCor($event)">Mudar cor!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isPremium: true,
+    };
+  },
+  props: {
+    cliente: Object,
+    showIdade: Boolean,
+  },
+  methods: {
+    mudarCor: function ($event) {
+      // console.log("Chamando evento!");
+      console.log($event);
+      this.isPremium = !this.isPremium; // sempre utilizar this quando referenciar dados dentro de um componente
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* scoped -> apenas este arquivo */
+.cliente {
+  background: rgb(191, 161, 161);
+  max-width: 400px;
+  padding: 1%;
+  margin-bottom: 2%;
+}
+
+.clientePremium {
+  background: rgb(7, 7, 7);
+  color: yellow;
+  max-width: 400px;
+  padding: 1%;
+  margin-bottom: 2%;
+}
+</style>
---
+++ S20 - Vue.js/09_formulario/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="app">
+    <h1>Guia Clientes</h1>
+
+    <div class="vFor" v-for="(cliente, index) in forClientes" :key="cliente.id">
+      <!-- <p>Testando v-for</p> -->
+      <h4>{{index + 1}}</h4>
+      <Cliente :cliente="cliente" />
+      <hr>
+      <p> Edição: </p>
+      <input type="text" v-model="cliente.nome">
+      <input type="text" v-model="cliente.email">
+    </div>
+  </div>
+</template>
+
+<script>
+import Cliente from "./components/Cliente.vue";
+export default {
+  name: "App",
+  data() {
+    return {
+      // cliente: {
+      //   nome: "nome do cliente",
+      //   email: "email_do_cliente@email.com",
+      //   idade: 19,
+      // },
+      forClientes: [
+        {
+          id: 1,
+          nome: "Vinicius",
+          email: "vinicius@email.com",
+          idade: 28,
+        },
+        {
+          id: 2,
+          nome: "Rebecka",
+          email: "rebecka@email.com",
+          idade: 24,
+        },
+        {
+          id: 3,
+          nome: "Malu",
+          email: "malu@email.com",
+          idade: 9,
+        },
+      ],
+    };
+  },
+  components: {
+    Cliente,
+  },
+};
+</script>
+
+<style></style>
