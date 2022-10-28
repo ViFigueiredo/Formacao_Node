d7b36b76f6f4b8d9a927b3031153e1af2ad35f20
Vinicius Figueiredo <vifigcode@gmail.com>
2022-10-28 14:20:51 -0300

    lista de pokemons + computed nome

---
+++ S21 - projeto_pokedex_vuejs/src/components/Pokemon.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ num }} {{ nomePokemonPriMaisc }}</h1>
+    <small>{{ url }}</small>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    num: Number,
+    name: String,
+    url: String,
+  },
+  computed: {
+    nomePokemonPriMaisc() {
+      var newName = this.name[0].toUpperCase() + this.name.slice(1);
+      return newName;
+    },
+  },
+};
+</script>
+
+<style></style>
