c6371257ccf88c7f60d5ea167a1add7ebd03bb8d
Vinicius Figueiredo <vifigcode@gmail.com>
2022-10-28 12:09:51 -0300

    framework css bulma vue

---
+++ S20 - Vue.js/14_frameworks_css_vue/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app">
+    <!-- teste bulma css -->
+    <div class="buttons">
+      <button class="button is-info">Info</button>
+      <button class="button is-success">Success</button>
+      <button class="button is-warning">Warning</button>
+      <button class="button is-danger">Danger</button>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
