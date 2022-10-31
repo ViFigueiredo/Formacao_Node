3ba1de8bb9d2b698bffa78afcbc611c037ddd249
Vinicius Figueiredo <vifigcode@gmail.com>
2022-10-31 11:54:09 -0300

    crud completo

--- S13 - Consumo_API_Axios/index.js
+++ S13 - Consumo_API_Axios/index.js
@@ -2,15 +2,15 @@ axios.get('http://localhost:3000/games').then(response => {
     var games = response.data;
     var list = document.getElementById('games');
 
-    var item = document.createElement('li');
-
     // console.log(games.length);
-    if (games.length == 0) {
-        item.innerHTML = 'Lista de games vazia.';
-        list.appendChild(item);
-    }
+    // if (games.length == 0) {
+    //     item.innerHTML = 'Lista de games vazia.';
+    //     list.appendChild(item);
+    // }
 
     games.forEach(game => {
+        var item = document.createElement('li');
+
         item.setAttribute('data-id', game.id);
         item.setAttribute('data-title', game.title);
         item.setAttribute('data-year', game.year);
@@ -24,7 +24,14 @@ axios.get('http://localhost:3000/games').then(response => {
             deleteGame(item);
         });
 
+        var editBtn = document.createElement('button');
+        editBtn.innerHTML = 'Edit';
+        editBtn.addEventListener('click', () => {
+            loadForm(item);
+        });
+
         item.appendChild(deleteBtn);
+        item.appendChild(editBtn);
         list.appendChild(item);
         // console.log(game);
     });
@@ -32,6 +39,45 @@ axios.get('http://localhost:3000/games').then(response => {
 }).catch(error => {
     console.log(error);
 });
+
+function loadForm(itemForm) {
+    // console.log(game);
+
+    var id = itemForm.getAttribute('data-id');
+    var title = itemForm.getAttribute('data-title');
+    var year = itemForm.getAttribute('data-year');
+    var price = itemForm.getAttribute('data-price');
+
+    document.getElementById('idEdit').value = id;
+    document.getElementById('titleEdit').value = title;
+    document.getElementById('yearEdit').value = year;
+    document.getElementById('priceEdit').value = price;
+}
+
+function editGame() {
+    var idInput = document.getElementById('idEdit');
+    var titleInput = document.getElementById('titleEdit');
+    var yearInput = document.getElementById('yearEdit');
+    var priceInput = document.getElementById('priceEdit');
+
+    var game = {
+        id: idInput.value,
+        title: titleInput.value,
+        year: yearInput.value,
+        price: priceInput.value
+    }
+
+    var id = idInput.value;
+
+    axios.put('http://localhost:3000/game/' + id, game).then(response => {
+        if (response.status == 200) {
+            alert('Game atualizado!');
+            location.reload();
+        }
+    }).catch(error => {
+        console.log(error);
+    });
+}
 
 function deleteGame(ListItem) {
     var id = ListItem.getAttribute('data-id');
