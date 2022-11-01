d842732720e5dad9299898fddbfe7770c9c34e99
Vinicius Figueiredo <vifigcode@gmail.com>
2022-11-01 10:56:04 -0300

    logica de validação

--- S18 - Validacao_Formulario/index.js
+++ S18 - Validacao_Formulario/index.js
@@ -19,8 +19,46 @@ app.use(session({
 app.use(flash());
 
 app.get('/', (req, res) => {
-    console.log('Exec...');
-    res.send('Executando...');
+    // console.log('Exec...');
+    // res.send('Executando...');
+    res.render('index');
+});
+
+app.post('/form', (req, res) => {
+    let { email, nome, pontos } = req.body;
+
+    let emailError;
+    let pontosError;
+    let nomeError;
+
+    if (email == undefined || email == '') {
+        emailError = 'E-mail não pode ser vazio!'
+        console.log(emailError);
+    }
+
+    if (pontos == undefined || pontos < 20) {
+        pontosError = 'Você não pode ter menos de 20 pontos!'
+        console.log(pontosError);
+    }
+
+    if (nome == undefined || nome == '') {
+        nomeError = 'Nome não pode ser vazio!'
+        console.log(nomeError);
+    }
+
+    if (nome.length < 3) {
+        nomeError = 'Nome muito pequeno!'
+        console.log(nomeError);
+    }
+
+    if (emailError != undefined || pontosError != undefined || nomeError != undefined) {
+        // res.send('Preencha o formulário corretamente!')
+        res.redirect('/');
+    } else {
+        res.send('Formulário enviado com sucesso!');
+    }
+
+    // res.send(req.body);
 });
 
 app.listen(PORT, (req, res) => {
