822ac0ad8ed0b38afd7dfe3c3528f2ffc14f3357
Vinicius Figueiredo <vifigcode@gmail.com>
2022-11-01 13:45:47 -0300

    recaptcha, validações, ejs, flash-messages

--- S18 - Validacao_Formulario/index.js
+++ S18 - Validacao_Formulario/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
@@ -5,7 +6,8 @@ const cookieParser = require('cookie-parser');
 const flash = require('express-flash');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
+const recaptcha = process.env.RECAPTCHA_KEY;
 
 app.set('view engine', 'ejs');
 
@@ -21,9 +23,32 @@ app.use(session({
 app.use(flash());
 
 app.get('/', (req, res) => {
+
+    let emailError = req.flash('emailError');
+    let nomeError = req.flash('nomeError');
+    let pontosError = req.flash('pontosError');
+
+    let email = req.flash('email');
+    let nome = req.flash('nome');
+    let pontos = req.flash('pontos');
+
+    emailError = (emailError == undefined || emailError.length == 0) ? undefined : emailError;
+    nomeError = (nomeError == undefined || nomeError.length == 0) ? undefined : nomeError;
+    pontosError = (pontosError == undefined || pontosError.length == 0) ? undefined : pontosError;
+
+    email = (email == undefined || email.length == 0) ? undefined : email;
+    nome = (nome == undefined || nome.length == 0) ? undefined : nome;
+    pontos = (pontos == undefined || pontos.length == 0) ? undefined : pontos;
+
     // console.log('Exec...');
     // res.send('Executando...');
-    res.render('index');
+    res.render('index', {
+        emailError, nomeError, pontosError,
+        email: email,
+        nome: nome,
+        pontos: pontos,
+        recaptcha
+    });
 });
 
 app.post('/form', (req, res) => {
@@ -34,26 +59,35 @@ app.post('/form', (req, res) => {
     let nomeError;
 
     if (email == undefined || email == '') {
-        emailError = 'E-mail não pode ser vazio!'
-        console.log(emailError);
+        emailError = 'E-mail vazio!'
+        // console.log(emailError);
     }
 
     if (pontos == undefined || pontos < 20) {
-        pontosError = 'Você não pode ter menos de 20 pontos!'
-        console.log(pontosError);
+        pontosError = 'Pontos < 20!'
+        // console.log(pontosError);
     }
 
     if (nome == undefined || nome == '') {
-        nomeError = 'Nome não pode ser vazio!'
-        console.log(nomeError);
+        nomeError = 'Nome vazio!'
+        // console.log(nomeError);
     }
 
     if (nome.length < 3) {
         nomeError = 'Nome muito pequeno!'
-        console.log(nomeError);
+        // console.log(nomeError);
     }
 
     if (emailError != undefined || pontosError != undefined || nomeError != undefined) {
+
+        req.flash('emailError', emailError);
+        req.flash('nomeError', nomeError);
+        req.flash('pontosError', pontosError);
+
+        req.flash("email", email);
+        req.flash("nome", nome);
+        req.flash("pontos", pontos);
+
         // res.send('Preencha o formulário corretamente!')
         res.redirect('/');
     } else {
