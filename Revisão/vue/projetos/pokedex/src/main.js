fa99a284ed32cf170ef96045ccb30ec69e98945c
Vinicius Figueiredo <vifigcode@gmail.com>
2022-12-26 12:53:29 -0300

    init projeto pokedex

---
+++ Revisão/vue/projetos/pokedex/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+/* importa o framework CSS Bulma */
+import 'bulma/css/bulma.css';
+
+import './assets/main.css';
+
+createApp(App).mount('#app');
